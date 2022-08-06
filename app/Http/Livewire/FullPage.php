fc60c9cb1da768a98c26e09a36d0c427ae3a2231
dennisrmz <orellanadennis12@gmail.com>
2022-08-05 21:39:35 -0600

    assets

---
+++ app/Http/Livewire/FullPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FullPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.full-page');
+    }
+}
